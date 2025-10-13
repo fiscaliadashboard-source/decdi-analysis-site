82fe8f48753ea245d5fd3694faf6645869ca5803
fiscaliadashboard-source <fiscaliadashboard@gmail.com>
2025-10-13 13:10:12 -0500

    Delete src directory

--- src/config.js
+++
@@ -1,4 +0,0 @@
-export const BACKEND_URL = "https://decdi-backend-service.onrender.com";
-export const VALID_USER = "admin";
-export const VALID_PASS = "decdi2025";
-export const API_KEY = import.meta.env.VITE_API_KEY;
