5694cabcc8f6f9621aa04304e2d2933d8719c7db
fiscaliadashboard <fiscaliadashboard@gmail.com>
2025-10-13 15:37:15 -0500

    ***

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/decdi-analysis-site/'
+  base: '/decdi-analysis-site/',
 })
